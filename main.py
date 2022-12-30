365c8b944214737f2f8239f00705e9954e32098d
SaigoNoo <giorgiodoussis@gmail.com>
2022-12-30 15:40:50 +0100

    Function get_channel_stat() changed, and pandas lib removed

--- main.py
+++ main.py
@@ -1,18 +1,13 @@
-import pandas as pd
 import requests
 
-key = "TYPE YOUR TOKEN HERE AND ENABLE YouTube API V3"
+key = "AIzaSyCAqZoTJRfjgvpIJsuTuXsqRzF6tbfxnsI"
 channel = "UCTmzT0st55PREearcmcCnNQ"
 
 
 def get_channel_stat() -> dict:
     url = f'https://www.googleapis.com/youtube/v3/channels?part=statistics&id={channel}&key={key}'
-    print(url)
-
-    r = requests.get(url)
-    data = r.json()
-    return data['items'][0]['statistics']
-
+    jsonData = requests.get(url).json()
+    return jsonData['items'][0]['statistics']
 
 stats = get_channel_stat()
-pd.DataFrame([stats])
+print(stats["subscriberCount"])
